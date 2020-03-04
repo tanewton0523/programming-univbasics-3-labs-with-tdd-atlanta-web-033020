58a3dce507a8a4232bfc44c2badb3dd41f8418cf
TeMecha Newton <tanewton0523@gmail.com>
2020-03-04 14:29:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,7 @@
 first_number = 5
 second_number = 5
+
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
